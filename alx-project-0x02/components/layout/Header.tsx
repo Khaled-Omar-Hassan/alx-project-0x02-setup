109c7cd1565ec3af4271dce9c4c891e6456f9fab
Khaled Omar <khaledomar3112@gmail.com>
2025-06-08 04:34:11 +0300

    posts

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -12,6 +12,9 @@ const Header: React.FC = () => {
                         <li className="hover:underline">
                             <Link href="/about">About</Link>
                         </li>
+                        <li className="hover:underline">
+                            <Link href="/posts">Posts</Link>
+                        </li>
                     </ul>
                 </nav>
             </div>
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,8 @@
+export default function Posts() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold mb-4">Posts</h1>
+            <p className="text-lg text-gray-700">This is the posts page of our Next.js application.</p>
+        </div>
+    );
+}
