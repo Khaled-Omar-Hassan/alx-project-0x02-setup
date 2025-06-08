bb1fb1413a211d28d2bdd04ae881578ed8d4578b
Khaled Omar <khaledomar3112@gmail.com>
2025-06-08 04:36:34 +0300

    <>

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,13 +1,17 @@
 import Card from '@/components/common/Card';
+import Header from '@/components/layout/Header';
 
 export default function HomePage() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-            <p className="text-lg text-gray-700">This is a simple Next.js application.</p>
-            <Card title='title1' content='content1' />
-            <Card title='title2' content='content2' />
-            <Card title='title3' content='content3' />
-        </div>
+        <>
+            <Header />
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+                <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+                <p className="text-lg text-gray-700">This is a simple Next.js application.</p>
+                <Card title='title1' content='content1' />
+                <Card title='title2' content='content2' />
+                <Card title='title3' content='content3' />
+            </div>
+        </>
     );
 }
--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,8 +1,14 @@
+import Header from "@/components/layout/Header"
+import Head from "next/head"
+
 export default function Posts() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Posts</h1>
-            <p className="text-lg text-gray-700">This is the posts page of our Next.js application.</p>
-        </div>
+        <>
+            <Header />
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+                <h1 className="text-4xl font-bold mb-4">Posts</h1>
+                <p className="text-lg text-gray-700">This is the posts page of our Next.js application.</p>
+            </div>
+        </>
     );
 }
